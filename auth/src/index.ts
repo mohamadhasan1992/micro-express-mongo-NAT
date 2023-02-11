77b5666b9dffda8c90593a2f3ee349c6bcbd1471
Mohamadhasan Tabrizi <tabrizi.mohamadhasan@yahoo.com>
2023-02-11 08:08:43 +0330

    NEW errorhandling and auth routes

---
+++ auth/src/index.ts
@@ -0,0 +1,36 @@
+import express from "express";
+import {json} from "body-parser";
+import { currentUserRouter } from "./routes/current-user";
+import { signinRouter } from "./routes/signin";
+import { signoutRouter } from "./routes/signout";
+import { signupRouter } from "./routes/signup";
+import { errorHandler } from "./middlewares/error-handlers";
+import { NotFoundError } from "./errors/not-found-error";
+
+
+
+const app = express();
+
+
+
+app.use(json());
+
+
+app.get("/", (req,res) => {
+    res.send('hello')
+})
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+
+
+app.all('*', async(req, res, next) => {
+    next(new NotFoundError());
+})
+
+app.use(errorHandler)
+
+app.listen(3000, () => {
+    console.log('auth is listening on port 30000!!!!')
+})
