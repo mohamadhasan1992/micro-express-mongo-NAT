58eb8667e3ba768182f872ec85a0789ea7cfbe60
Mohamadhasan Tabrizi <tabrizi.mohamadhasan@yahoo.com>
2023-02-11 19:32:41 +0330

    NEW client side

---
+++ client/api/build-client.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+export default ({ req }) => {
+  if (typeof window === 'undefined') {
+    // We are on the server
+
+    return axios.create({
+      // baseURL: 'http://ingress-nginx.ingress-nginx.svc.cluster.local',
+      baseURL: 'http://localhost:30899',
+      headers: req.headers
+    });
+  } else {
+    // We must be on the browser
+    return axios.create({
+      baseUrl: '/'
+    });
+  }
+};
